a537b188d8fbaea1ef1589877af9ff6fbe5b6f45
Piwero <piwero@gmail.com>
2021-03-28 11:21:39 +0100

    fix dependencies

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express();
 const port = 8000;
 
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
